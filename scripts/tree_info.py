c91e4a3022b55845e8482fe6aa8ffc64d44e3df8
Daniel Doerr <daniel.doerr@epfl.ch>
2017-02-16 09:53:22 +0100

    included also coverage information in tree_info

--- scripts/tree_info.py
+++ scripts/tree_info.py
@@ -1,11 +1,43 @@
 #!/usr/bin/env python
 
 from sys import stdout, stderr, exit
+from os.path import join, dirname, isfile
+from itertools import izip, chain
 from optparse import OptionParser
 import shelve
+import re
+
+from pairwise_similarities import readGenomeMap, GM_ACTV_GNS_KEY, \
+        GENOME_MAP_FILE
 from psycho import node
 
+PAT_CHR = re.compile('.*\|chromosome\|([^\|]+)(\|.*|$)')
+PAT_POS = re.compile('.*\|(\d+):(\d+)(\|.*|$)')
 
+
+def compute_coverage(root, gene_orders, g2pos, marker_loc, ref):
+    covered_markers = list(set() for _ in gene_orders[0])
+
+    queue = [(root, None)]
+    while queue:
+        u, u_id = queue.pop()
+        # remember found u_id in top-down traversal 
+        if u.id != None:
+            u_id = u.id
+        if u_id != None and u.links:
+            gos = gene_orders[u_id]
+            gos2pos = g2pos[u_id]
+            for x, gx1, gx2 in chain(((ref, u.intt[0], u.intt[1]),), u.links):
+                covered_markers[x].update(gos[x][gos2pos[x][gx1]:gos2pos[x][gx2]+1])
+        else:
+            for child in u.children:
+                if child.children:
+                    queue.append((child, u_id))
+    res = list()
+    for x in xrange(len(covered_markers)):
+        res.append(sum(map(lambda z: marker_loc[x][z][1] -
+            marker_loc[x][z][0]+1, covered_markers[x])))
+    return res
 
 if __name__ == '__main__':
     usage = '%prog [options] <SHELVE>'
@@ -26,16 +58,56 @@ if __name__ == '__main__':
     # load inclusion tree
     #
     root = shObj['inclusion_tree']
+    genomes = shObj['genomes']
     
-    queue = list(map(lambda x: (x, 1), root.children))
+    queue = [(root, 1, True)]
     depths = list()
+    syn_blocks = 0
+    top_level = 0
     while queue:
-        u, u_depth = queue.pop()
+        u, u_depth, isTopLevel = queue.pop()
+        if u.links:
+            syn_blocks += 1
+            if isTopLevel and u.children:
+                top_level += 1
+                isTopLevel = False
         for child in u.children:
             if len(child.children) > 1:
-                queue.append((child, u_depth+1))
+                queue.append((child, u_depth+1, isTopLevel))
             else:
                 depths.append(u_depth+1)
 
     print >> stdout, 'max tree depth: %s' %max(depths)
     print >> stdout, 'avg tree depth: %s' %(sum(depths)/float(len(depths)))
+    print >> stdout, '# internal nodes w. links: %s' %syn_blocks
+    print >> stdout, '# top-level blocks: %s' %top_level
+
+    #
+    # read marker positions from marker fasta files
+    #
+    gMapF = join(dirname(shObj['orig_pw_dists'][0]), GENOME_MAP_FILE)
+    if not isfile(gMapF):
+        print >> stderr, ('ERROR: unable to find genome map at assumed ' + \
+                'location %s. Exiting') %gMapF
+        exit(1)
+
+    gMap = readGenomeMap(open(gMapF)) 
+    marker_loc = list()
+    for Gx in genomes:
+        m_dict = dict()
+        for x in xrange(len(gMap[Gx][GM_ACTV_GNS_KEY])):
+            gx1 = gMap[Gx][GM_ACTV_GNS_KEY][x]
+            m_dict[(PAT_CHR.match(gx1).group(1), x+1)] = map(int, \
+                    PAT_POS.match(gx1).group(1,2))
+        marker_loc.append(m_dict)
+           
+    gene_orders = shObj['gene_orders']
+    g2pos = [[dict(izip(go, xrange(len(go)))) for go in gos] for gos in \
+            gene_orders]
+
+    coverage = compute_coverage(root, gene_orders, g2pos, marker_loc, shObj['ref'])
+    print >> stdout, 'coverage:'
+    for x in xrange(len(coverage)):
+        print >> stdout, '\t%s:\t%s' %(genomes[x], coverage[x])
+
+
